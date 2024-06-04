237a36fd25d454c66c1b1ac532a21f0bf6d4b549
eliasdevmin <eliasaraujo1222@outlook.com>
2024-06-03 23:39:18 -0300

    ajuste a porta 5000

--- app.py
+++ app.py
@@ -70,4 +70,5 @@ api.add_resource(UserLogin, '/login')
 api.add_resource(Protected, '/protected')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+
