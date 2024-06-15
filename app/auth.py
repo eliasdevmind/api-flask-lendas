53503d3375cc711df5a9ba560348c65a2a0ddc55
eliasdevmin <eliasaraujo1222@outlook.com>
2024-06-15 00:07:08 -0300

    teste

--- app/auth.py
+++ app/auth.py
@@ -14,14 +14,20 @@ class UserRegister(Resource):
             password = generate_password_hash(data['password'])
 
             cursor = mysql.connection.cursor()
+            cursor.execute("SELECT * FROM users WHERE username = %s OR email = %s", (username, email))
+            existing_user = cursor.fetchone()
+            if existing_user:
+                cursor.close()
+                return jsonify({"message": "Usuário já registrado!"}), 400
+
             cursor.execute("INSERT INTO users(username, email, password) VALUES (%s, %s, %s)", (username, email, password))
             mysql.connection.commit()
             cursor.close()
 
-            return jsonify({"message": "User registered successfully!"})
+            return jsonify({"message": "Registrado com sucesso!"})
         except Exception as e:
-            logging.error(f"Error registering user: {e}")
-            return jsonify({"message": "Error registering user"}), 500
+            logging.error(f"Erro ao registrar usuário: {e}")
+            return jsonify({"message": "Erro ao registrar usuário"}), 500
 
 class UserLogin(Resource):
     def post(self):
@@ -37,12 +43,12 @@ class UserLogin(Resource):
 
             if user and check_password_hash(user[3], password):
                 access_token = create_access_token(identity={'username': user[1]})
-                return jsonify({"access_token": access_token})
+                return jsonify({"message": "Login realizado com sucesso!", "access_token": access_token})
             else:
-                return jsonify({"message": "Invalid credentials!"}), 401
+                return jsonify({"message": "Usuário ou senha incorretos!"}), 401
         except Exception as e:
-            logging.error(f"Error logging in: {e}")
-            return jsonify({"message": "Internal server error"}), 500
+            logging.error(f"Erro ao fazer login: {e}")
+            return jsonify({"message": "Erro interno do servidor"}), 500
 
 class Protected(Resource):
     @jwt_required()
